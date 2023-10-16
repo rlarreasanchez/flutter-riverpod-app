49ebe6b1dad8d5e07f5bffba5c7ede12487b8821
Rafael Larrea Sánchez <rlarreasanchez@gmail.com>
2023-10-16 15:13:58 +0200

    Se implementan los todos providers

---
+++ lib/presentation/providers/todos_providers.dart
@@ -0,0 +1,84 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:riverpod_app/config/config.dart';
+import 'package:riverpod_app/domain/domain.dart';
+import 'package:uuid/uuid.dart';
+
+part 'todos_providers.g.dart';
+
+const uuid = Uuid();
+
+enum TodoFilterType { all, completed, pending }
+
+@Riverpod(keepAlive: true)
+class TodoCurrentFilter extends _$TodoCurrentFilter {
+  @override
+  TodoFilterType build() => TodoFilterType.all;
+
+  void setCurrentFilter(TodoFilterType newFilter) {
+    state = newFilter;
+  }
+}
+
+@Riverpod(keepAlive: true)
+class Todos extends _$Todos {
+  @override
+  List<Todo> build() => [
+        Todo(
+            id: uuid.v4(),
+            description: RandomGenerator.getRandomName(),
+            completedAt: null),
+        Todo(
+            id: uuid.v4(),
+            description: RandomGenerator.getRandomName(),
+            completedAt: DateTime.now()),
+        Todo(
+            id: uuid.v4(),
+            description: RandomGenerator.getRandomName(),
+            completedAt: null),
+        Todo(
+            id: uuid.v4(),
+            description: RandomGenerator.getRandomName(),
+            completedAt: DateTime.now()),
+        Todo(
+            id: uuid.v4(),
+            description: RandomGenerator.getRandomName(),
+            completedAt: null),
+        Todo(
+            id: uuid.v4(),
+            description: RandomGenerator.getRandomName(),
+            completedAt: null),
+      ];
+
+  void createTodo(String description) {
+    state = [
+      ...state,
+      Todo(id: uuid.v4(), description: description, completedAt: null)
+    ];
+  }
+
+  void toggleTodo(String id) {
+    state = state.map((todo) {
+      if (todo.id == id) {
+        todo = todo.copyWith(completedAt: todo.done ? null : DateTime.now());
+      }
+      return todo;
+    }).toList();
+  }
+}
+
+@riverpod
+List<Todo> filteredTodos(FilteredTodosRef ref) {
+  final currentFilter = ref.watch(todoCurrentFilterProvider);
+  final todos = ref.watch(todosProvider);
+
+  switch (currentFilter) {
+    case TodoFilterType.all:
+      return todos;
+
+    case TodoFilterType.completed:
+      return todos.where((todo) => todo.done).toList();
+
+    case TodoFilterType.pending:
+      return todos.where((todo) => !todo.done).toList();
+  }
+}
---
+++ lib/presentation/providers/todos_providers.g.dart
@@ -0,0 +1,55 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'todos_providers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$filteredTodosHash() => r'e1230c687e407191c462b6449f92f529f40eb6d6';
+
+/// See also [filteredTodos].
+@ProviderFor(filteredTodos)
+final filteredTodosProvider = AutoDisposeProvider<List<Todo>>.internal(
+  filteredTodos,
+  name: r'filteredTodosProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$filteredTodosHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef FilteredTodosRef = AutoDisposeProviderRef<List<Todo>>;
+String _$todoCurrentFilterHash() => r'ab9e7ca6a4ed8cc6c0cb7ecb24d4cb2e3ef806e3';
+
+/// See also [TodoCurrentFilter].
+@ProviderFor(TodoCurrentFilter)
+final todoCurrentFilterProvider =
+    NotifierProvider<TodoCurrentFilter, TodoFilterType>.internal(
+  TodoCurrentFilter.new,
+  name: r'todoCurrentFilterProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$todoCurrentFilterHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$TodoCurrentFilter = Notifier<TodoFilterType>;
+String _$todosHash() => r'705647f346de771cbd1d02823529c3edeffdb2a9';
+
+/// See also [Todos].
+@ProviderFor(Todos)
+final todosProvider = NotifierProvider<Todos, List<Todo>>.internal(
+  Todos.new,
+  name: r'todosProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$todosHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$Todos = Notifier<List<Todo>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
