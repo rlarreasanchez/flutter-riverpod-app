2fefa71bf5f4c5778ab37ca34a35474d68cb5ba3
Rafael Larrea Sánchez <rlarreasanchez@gmail.com>
2023-10-16 13:08:56 +0200

    Se utiliza riverpod para provider y provider + gorouter

---
+++ lib/presentation/providers/hello_world_provider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'hello_world_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$helloworldHash() => r'651763d44b0d131e17915d671a1922d141de64d4';
+
+/// See also [helloworld].
+@ProviderFor(helloworld)
+final helloworldProvider = AutoDisposeProvider<String>.internal(
+  helloworld,
+  name: r'helloworldProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$helloworldHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef HelloworldRef = AutoDisposeProviderRef<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
