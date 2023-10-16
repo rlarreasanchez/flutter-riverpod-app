9781756ea961b17fa7ced19d0d436369baedad8e
Rafael Larrea Sánchez <rlarreasanchez@gmail.com>
2023-10-16 17:30:15 +0200

    Se implementa el stream provider

--- lib/presentation/screens/07_stream_provider/stream_provider_screen.dart
+++ lib/presentation/screens/07_stream_provider/stream_provider_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_app/presentation/providers/stream_provider.dart';
 
 class StreamProviderScreen extends StatelessWidget {
   const StreamProviderScreen({super.key});
@@ -10,6 +11,39 @@ class StreamProviderScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Stream Provider'),
       ),
+      body: const Center(child: StreamView()),
+    );
+  }
+}
+
+class StreamView extends ConsumerWidget {
+  const StreamView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final usersInChatAsync = ref.watch(usersInChatProvider);
+
+    if (usersInChatAsync.isLoading) {
+      return const CircularProgressIndicator();
+    }
+
+    if (usersInChatAsync.hasError) {
+      return Center(
+        child: Text('$usersInChatAsync.error'),
+      );
+    }
+
+    final users = usersInChatAsync.value!;
+
+    return ListView.builder(
+      itemCount: users.length,
+      itemBuilder: (BuildContext context, int index) {
+        final user = users[index];
+
+        return ListTile(
+          title: Text(user),
+        );
+      },
     );
   }
 }
